04f786e56af57f31fff3a36256f3220075bcb388
Felix Delval <fdelval@adobe.com>
2024-08-06 14:16:39 +0200

    Hard coding the configuration

--- scripts/configs.js
+++ scripts/configs.js
@@ -36,18 +36,92 @@ function buildConfigURL(environment) {
 }
 
 const getConfigForEnvironment = async (environment) => {
-  const env = environment || calcEnvironment();
-  let configJSON = window.sessionStorage.getItem(`config:${env}`);
-  if (!configJSON) {
-    configJSON = await fetch(buildConfigURL(env));
-    if (!configJSON.ok) {
-      throw new Error(`Failed to fetch config for ${env}`);
-    }
-    configJSON = await configJSON.text();
-    window.sessionStorage.setItem(`config:${env}`, configJSON);
-  }
-  return configJSON;
-};
+  return `
+{
+"total": 19,
+"offset": 0,
+"limit": 19,
+"data": [
+{
+"key": "commerce-endpoint",
+"value": "https://catalog-service.adobe.io/graphql"
+},
+{
+"key": "commerce-environment-id",
+"value": "f38a0de0-764b-41fa-bd2c-5bc2f3c7b39a"
+},
+{
+"key": "commerce-website-code",
+"value": "base"
+},
+{
+"key": "commerce-store-view-code",
+"value": "default"
+},
+{
+"key": "commerce-store-code",
+"value": "main_website_store"
+},
+{
+"key": "commerce-customer-group",
+"value": "b6589fc6ab0dc82cf12099d1c2d40ab994e8410c"
+},
+{
+"key": "commerce-x-api-key",
+"value": "4dfa19c9fe6f4cccade55cc5b3da94f7"
+},
+{
+"key": "commerce-core-endpoint",
+"value": "https://mcprod.aemshop.net/graphql"
+},
+{
+"key": "commerce-root-category-id",
+"value": "2"
+},
+{
+"key": "commerce-environment",
+"value": "Production"
+},
+{
+"key": "commerce-store-id",
+"value": "1"
+},
+{
+"key": "commerce-store-name",
+"value": "Main Website Store"
+},
+{
+"key": "commerce-store-url",
+"value": "https://mcprod.aemshop.net/"
+},
+{
+"key": "commerce-store-view-id",
+"value": "1"
+},
+{
+"key": "commerce-store-view-name",
+"value": "Default Store View"
+},
+{
+"key": "commerce-website-id",
+"value": "1"
+},
+{
+"key": "commerce-website-name",
+"value": "Main Website"
+},
+{
+"key": "commerce-base-currency-code",
+"value": "USD"
+},
+{
+"key": "adyen-client-key",
+"value": "test_TBG272DDJZH4ZAAXSBAKQZ44ZQC6LWOU"
+}
+],
+":type": "sheet"
+}`
+}
 
 /**
  * This function retrieves a configuration value for a given environment.
@@ -62,3 +136,5 @@ export const getConfigValue = async (configParam, environment) => {
   const configElements = JSON.parse(configJSON).data;
   return configElements.find((c) => c.key === configParam)?.value;
 };
+
+
